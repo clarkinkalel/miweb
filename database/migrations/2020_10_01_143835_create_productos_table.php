5fc0e0386b508e106fd96c0e0f9ec4858a10375a
clarkinkael <larkinrios@gmail.com>
2020-10-02 03:37:44 -0500

    se agrego base de datos

---
+++ database/migrations/2020_10_01_143835_create_productos_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id')->unsigned();
+            $table->text('comentarios');
+            $table->string('nombre');
+            $table->mediumText('descipcion');
+            $table->double('precio');
+            $table->smallInteger('votos');
+            $table->time('sunrise');
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
